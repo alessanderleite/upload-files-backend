47ee30d695d969a1c91784acb0f4943a7236ab59
Alessander <alessander.logistica@gmail.com>
2020-05-15 23:41:47 -0300

    create the interface

---
+++ src/main/java/com/example/app/FilesStorageService.java
@@ -0,0 +1,16 @@
+package com.example.app;
+
+import java.nio.file.Path;
+import java.util.stream.Stream;
+
+import org.springframework.core.io.Resource;
+import org.springframework.web.multipart.MultipartFile;
+
+public interface FilesStorageService {
+	
+	void init();
+	void save(MultipartFile file);
+	Resource load(String filename);
+	void deleteAll();
+	Stream<Path> loadAll();
+}
